8eb44015161b8b917da841b67cee5ba06dcfb594
Ryan <guan.ryan@gmail.com>
2020-05-04 22:22:59 -0400

    merged datasets.py from master

--- datasets.py
+++ datasets.py
@@ -30,9 +30,15 @@ class RecursionDataset(Dataset):
 
         self.csv_df = load_metadata_df(csv_path)
 
+        # NORMALIZATION: Temporary location (feel free to move somewhere else)
+        if args.normalization == 'plate':
+          self.stats = pd.read_csv(os.path.join(args.data_dir, 'plate_stats.csv')) #ideally csv filename would not be hardcoded
+        elif args.normalization == 'experiment':
+          self.stats = pd.read_csv(os.path.join(args.data_dir, 'exp_stats.csv'))
+
         if sirnas_to_keep is not None:
             self.csv_df = self.csv_df[self.csv_df['sirna'].isin(sirnas_to_keep)]
-
+            
         # Initialize cell type encoders
         self.cell_type_label_encoder = preprocessing.LabelEncoder()
         cell_type_encoded = self.cell_type_label_encoder.fit_transform(self.csv_df['cell_type'])
@@ -91,6 +97,24 @@ class RecursionDataset(Dataset):
 
         return_x = self.create_img_tensor(experiment, plate_num, well, site)
 
+        if args.normalization == 'plate':
+          # Get all channel where mean & std values are located
+          plate_bool = ( (self.stats['experiment']==experiment) & (self.stats['plate']==plate) )
+          # Create stack of all channel values
+          idx = self.stats.index[plate_bool] 
+
+          pixel_mean = torch.tensor(self.stats['mean'][idx].to_numpy()) # Get mean
+          pixel_std = torch.tensor(self.stats['std'][idx].to_numpy()) # Get std
+          return_x = (return_x - pixel_mean.reshape(-1,1,1)) / pixel_std.reshape(-1,1,1) # Normalize
+        elif args.normalization == 'experiment':
+          # Get all channel where mean & std values are located
+          exp_bool = (self.stats['experiment']==experiment)
+          # Create stack of all channel values
+          idx = self.stats.index[exp_bool]
+
+          pixel_mean = torch.tensor(self.stats['mean'][idx].to_numpy()) # Get mean
+          pixel_std = torch.tensor(self.stats['std'][idx].to_numpy()) # Get std
+          return_x = (return_x - pixel_mean.reshape(-1,1,1)) / pixel_std.reshape(-1,1,1) # Normalize
         return return_x, cell_type, sirna_label
 
     def __len__(self):
